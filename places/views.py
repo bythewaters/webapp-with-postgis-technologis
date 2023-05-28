2053f28e45455987af27ee73dd3e26049dd27ec7
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-28 21:11:41 +0200

    Added view PlaceListView for app place

--- places/views.py
+++ places/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from places.models import Place
+from places.serializers import PlaceListSerializer
+
+
+class PlaceListView(viewsets.ModelViewSet):
+    serializer_class = PlaceListSerializer
+    queryset = Place.objects.all()
+
